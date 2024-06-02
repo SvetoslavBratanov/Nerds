76d33d9f3a3d121288358709c961bae253ebe9f0
Svetoslav Bratanov <svetoslav@Svetoslavs-MacBook-Pro.local>
2024-06-02 13:18:09 +0300

    Add sum function

--- Nerds/src/main/java/bg/softuni/nerds/Main.java
+++ Nerds/src/main/java/bg/softuni/nerds/Main.java
@@ -2,6 +2,13 @@ package bg.softuni.nerds;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello, nerds!");
+
+        System.out.print(sum(4, 3));
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
     }
 }
