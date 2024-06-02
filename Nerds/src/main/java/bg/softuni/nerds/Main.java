095c6951c72baa17d07a1574b8ac42912b7d2ba9
AntoanetaCherkezova <aa.cherkezova@gmail.com>
2024-06-02 14:04:26 +0300

    Add new method divide

--- Nerds/src/main/java/bg/softuni/nerds/Main.java
+++ Nerds/src/main/java/bg/softuni/nerds/Main.java
@@ -3,5 +3,10 @@ package bg.softuni.nerds;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, nerds!");
+        System.out.println(divide(12, 5));
+    }
+
+    public static double divide(double a, double b){
+        return a/b;
     }
 }
