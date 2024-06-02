c83359ba1d6286c1830c8510e6413ed94671edd6
AntoanetaCherkezova <aa.cherkezova@gmail.com>
2024-06-02 11:27:33 +0300

    Init project

---
+++ Nerds/src/main/java/bg/softuni/nerds/NerdsApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.nerds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerdsApplication.class, args);
+	}
+
+}
---
+++ Nerds/src/test/java/bg/softuni/nerds/NerdsApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.nerds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NerdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
