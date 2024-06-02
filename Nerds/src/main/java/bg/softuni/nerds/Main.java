9413a10da53d384445a8d423cf3382d2572d90c3
borisasenov <borko9009@gmail.com>
2024-06-02 14:46:32 +0300

    commit

--- Nerds/src/main/java/bg/softuni/nerds/Main.java
+++ Nerds/src/main/java/bg/softuni/nerds/Main.java
@@ -6,11 +6,20 @@ public class Main {
         System.out.println("Hello, nerds!");
         System.out.print(sum(4, 3));
         System.out.println(divide(12, 5));
+        System.out.print(substract(10,3));
+
     }
 
     public static double divide(double a, double b){
         return a/b;
+
     }
+    public static int substract(int a, int b) {
+        int sub = a-b
+        return sub;
+    }
+
+
 
     public static int sum(int a, int b) {
         return a + b;
